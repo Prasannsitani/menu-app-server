f37fb7a69c5803c457ace5c40b9a0e89f9ba7131
Prasann Sitani <prasann.sitani@dstreet.finance>
2023-06-25 14:48:44 +0530

    feat: initial repo setup

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 8080
+
+app.get('/', (req, res) => {
+    res.send("Server is up!");
+})
+
+app.listen(PORT, (req, res) => {
+    console.log("listening on port " + PORT);
+})
