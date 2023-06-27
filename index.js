3fb1683f0ab0413d14fdab410e5a61fa4d871be7
Prasannsitani <prasannsitani000@gmail.com>
2023-06-27 11:30:21 +0530

    fix: db connection setup

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
-import express from 'express';
+import express from 'express'
+import mongoose from 'mongoose'
+import userModel from './models/user-model.js'
 
-const app = express();
+const app = express()
 const PORT = process.env.PORT || 8080
 
-app.get('/', (req, res) => {
-    res.send("Server is up!");
+const DB_URL =
+  'mongodb+srv://admin:admin@ar-menu-app-prod.kavmp2c.mongodb.net/restaurant?retryWrites=true&w=majority'
+
+mongoose
+  .connect(DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB Connected!!')
+  })
+  .catch(error => {
+    console.log('connection error : ', error)
+  })
+
+app.get('/', async (req, res) => {
+  try {
+    const data = await userModel.find()
+    console.log('data found : ', data)
+    // (error, user) => {
+    //     if (error) {
+    //       console.log('db error : ' + JSON.stringify(error))
+    //     }
+    //     res.json(user)
+    //   }
+  } catch (error) {
+    console.log('catch error : ' + error)
+  }
+
+  //   res.json({
+  //     error: null,
+  //     data: null,
+  //   })
 })
 
 app.listen(PORT, (req, res) => {
-    console.log("listening on port number " + PORT);
+  console.log('Server started on port ' + PORT)
 })
