79dc2529b125347d591ee74847e15abcd455f58c
Prasannsitani <prasannsitani000@gmail.com>
2023-06-29 12:11:24 +0530

    chore: updated user routes to auth routes

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import './config/database.js'
-import userRoutes from './routes/user-routes.js'
+import authRoutes from './routes/auth-routes.js'
 
 const app = express()
 const PORT = process.env.PORT || 8080
 
 app.use(express.json())
-app.use('/api/users', userRoutes)
+app.use('/api/v1/auth', authRoutes)
 
 app.listen(PORT, (req, res) => {
   console.log('Server started on port ' + PORT)
