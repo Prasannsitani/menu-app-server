9ec50ef177fcc30a18b7797a77994e336e60fdd5
Prasannsitani <prasannsitani000@gmail.com>
2023-06-29 12:39:41 +0530

    refactor: refactored auth routes and sgregate controller

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,34 +1,8 @@
 import express from 'express'
-import User from '../models/user-model.js'
-import Helper from '../utils/helper-functions.js'
+import { login } from '../controllers/auth-controller.js'
 
 const router = express.Router()
 
-router.get('/login', async (req, res, next) => {
-  try {
-    User.findOne({ mobile_number: req.body.mobile_number }).then(async user => {
-      if (user) {
-        const token = await Helper.jwtSignIn({ id: user.id })
-        res.status(200).json({
-          message: 'Login successful',
-          jwt: token,
-        })
-      } else {
-        const user = new User(req.body)
-        user
-          .save()
-          .then(async newUser => {
-            const token = await Helper.jwtSignIn({ id: newUser.id })
-            res.status(201).json({ message: 'Login successful', jwt: token })
-          })
-          .catch(error => {
-            res.status(500).json({ error: error._message })
-          })
-      }
-    })
-  } catch (error) {
-    res.status(500).json({ error: error._message })
-  }
-})
+router.get('/login', login)
 
 export default router
