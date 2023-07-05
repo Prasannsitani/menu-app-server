34324553d3196aeb3726f43eec2e4f9c5200cc7c
Prasannsitani <prasannsitani000@gmail.com>
2023-07-05 14:16:25 +0530

    fix: firebase admin configuration

--- index.js
+++ index.js
@@ -3,11 +3,20 @@ import './config/database.js'
 import authRoutes from './routes/auth-routes.js'
 import foodRoutes from './routes/food-routes.js'
 import authMiddleware from './middlewares/auth-middleware.js'
+import admin from 'firebase-admin'
+import ServiceAccountKey from './services-account-key.json'
 
 const app = express()
 const PORT = process.env.PORT || 8080
 
 app.use(express.json())
+
+// Firebase Configuration
+admin.initializeApp({
+  credential: admin.credential.cert(ServiceAccountKey),
+})
+
+// App Routes
 app.use('/api/v1/auth', authRoutes)
 app.use('/api/v1/food', authMiddleware, foodRoutes)
 
