db5b1513df6604293271d7b7c3769d64b6699405
Prasannsitani <prasannsitani000@gmail.com>
2023-07-06 15:37:27 +0530

    fix: session configuration

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from 'express'
+import session from 'express-session'
+import cookieParser from 'cookie-parser'
+import csurf from 'csurf'
+
 import './config/firebase.js'
 import './config/database.js'
 import authRoutes from './routes/auth-routes.js'
@@ -9,6 +13,31 @@ const app = express()
 const PORT = process.env.PORT || 8080
 
 app.use(express.json())
+app.use(cookieParser())
+app.use(express.urlencoded({ extended: true }))
+
+// Configure session
+app.use(
+  session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: true, // Ensures cookies are only sent over HTTPS
+      httpOnly: true, // Prevents client-side access to cookies
+      sameSite: 'strict', // Protects against cross-site request forgery (CSRF) attacks
+    },
+  }),
+)
+
+// Use CSRF protection
+app.use(csurf())
+app.use((err, req, res, next) => {
+  if (err.code !== 'EBADCSRFTOKEN') return next(err)
+
+  // Handle CSRF token validation failure
+  res.status(403).send('CSRF token validation failed')
+})
 
 // App Routes
 app.use('/api/v1/auth', authRoutes)
